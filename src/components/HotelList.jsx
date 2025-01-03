2263edf41c8ab3c756c7d9b2b1cca9a7f5aebc99
Mohammed Taysir Ali <118166509+alitaysir@users.noreply.github.com>
2025-01-04 00:04:39 +0530

    Update HotelList.jsx

--- src/components/HotelList.jsx
+++ src/components/HotelList.jsx
@@ -1,20 +1,26 @@
-"use client"
-import { blog_data } from '@/Assets/assets'
-import React from 'react'
-import HotelOne from './HotelOne'
+"use client";
+
+import { blog_data } from "@/Assets/assets"; // Ensure this path is correct
+import React from "react";
+import HotelOne from "./HotelOne";
 
 const HotelList = () => {
   return (
     <div>
-        <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24 '>
-            {
-                blog_data.map((item)=>{
-                   return <HotelOne key={item.id} image={item.image} title={item.title} description={item.description} category={item.category} id={item.id}/>
-                })
-            }
-        </div>
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        {blog_data.map((item) => (
+          <HotelOne
+            key={item.id}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+            category={item.category}
+            id={item.id}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HotelList
+export default HotelList;
