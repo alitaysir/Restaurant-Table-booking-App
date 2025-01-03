666112c994cd8928ebc55c01c7d0c78185b0aea5
Mohammed Taysir Ali <118166509+alitaysir@users.noreply.github.com>
2025-01-03 22:47:56 +0530

    Update HotelOne.jsx

--- src/components/HotelOne.jsx
+++ src/components/HotelOne.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const HotelOne = ({title,description,category,image,id}) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black'>
-      <Image src={image} alt="Description of the image" width={400} height={400} className='border-b border-black'/>
+      <Image src={image} alt="" width={400} height={400} className='border-b border-black'/>
       <p className='ml-2 mt-4 mb-2 px-1 bg-blue-100 text-gray-600 inline-block text-sm '>{category}</p>
       <h3 className='mb-2 text-center text-lg font-medium tracking-tight text-gray-900'>{title}</h3>
       <p className='mb-3 px-1 text-sm tracking-tight text-gray-600'>{description}</p>
